4f523967e5b8527769009a36383d2070adfb9487
masa <79982360+masatojo@users.noreply.github.com>
2022-02-18 17:02:25 +0900

    Update browser.js

--- browser.js
+++ browser.js
@@ -5,5 +5,8 @@
  *
  * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
  */
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const tslib_1=require("tslib"),core_1=tslib_1.__importDefault(require("@walletconnect/core")),utils_1=require("@walletconnect/utils"),cryptoLib=tslib_1.__importStar(require("@walletconnect/browser-crypto"));class WalletConnect extends core_1.default{constructor(e){super({cryptoLib:cryptoLib,connectorOpts:e}),utils_1.logDeprecationWarning()}}exports.default=WalletConnect;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});
+const tslib_1=require("tslib"),core_1=tslib_1.__importDefault(require("@walletconnect/core")),utils_1=require("@walletconnect/utils"),cryptoLib=tslib_1.__importStar(require("@walletconnect/browser-crypto"));
+class WalletConnect 
+  extends core_1.default{constructor(e){super({cryptoLib:cryptoLib,connectorOpts:e}),utils_1.logDeprecationWarning()}}exports.default=WalletConnect;
 //# sourceMappingURL=/sm/01c0a6137234966eeca0c937ffd8943cfca22e2ecdb013078966f2d98b7334a6.map
